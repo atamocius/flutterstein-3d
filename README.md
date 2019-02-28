30d9702d950fd0d4faa1dd9da2085602c57cf4ee
Daniel Anton Mata <anton.mata@gmail.com>
2019-03-01 03:00:23 +0800

    Adjusted resolution

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 A new Flutter project.
 
+- Eldritch horror survival shooter
+
 ## Getting Started
 
 This project is a starting point for a Flutter application.
@@ -11,6 +13,6 @@ A few resources to get you started if this is your first Flutter project:
 - [Lab: Write your first Flutter app](https://flutter.io/docs/get-started/codelab)
 - [Cookbook: Useful Flutter samples](https://flutter.io/docs/cookbook)
 
-For help getting started with Flutter, view our 
-[online documentation](https://flutter.io/docs), which offers tutorials, 
+For help getting started with Flutter, view our
+[online documentation](https://flutter.io/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,37 @@
 // https://gist.github.com/netsmertia/9c588f23391c781fa1eb791f0dce0768
 
-import 'dart:async';
 import 'dart:ui';
 import 'dart:typed_data';
 import 'dart:math' as math;
 
 import 'package:flutter/services.dart';
 
-final deviceTransform = Float64List(16)
-  ..[0] = window.devicePixelRatio
-  ..[5] = window.devicePixelRatio
-  ..[10] = 1.0
-  ..[15] = 1.0;
-
 const tau = math.pi * 2;
 
 main() async {
   await SystemChrome.setEnabledSystemUIOverlays([]);
   await SystemChrome.setPreferredOrientations(
-    [DeviceOrientation.landscapeLeft],
+    [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight],
   );
 
-  final initialSize = await Future<Size>(() {
-    if (window.physicalSize.isEmpty) {
-      final completer = Completer<Size>();
-      window.onMetricsChanged = () {
-        if (!window.physicalSize.isEmpty) {
-          completer.complete(window.physicalSize);
-        }
-      };
-      return completer.future;
-    }
-    return window.physicalSize;
-  });
-
-  print(window.physicalSize);
-  print(initialSize);
-  print(window.devicePixelRatio);
-  print(window.physicalSize / window.devicePixelRatio);
-  print(Offset.zero & (window.physicalSize / window.devicePixelRatio));
+  final pixelRatio = window.physicalSize.height / 360;
+  final deviceTransform = Float64List(16)
+    ..[0] = pixelRatio
+    ..[5] = pixelRatio
+    ..[10] = 1.0
+    ..[15] = 1.0;
 
   var previous = Duration.zero;
   final world = World();
 
   window.onBeginFrame = (now) {
-    final paintBounds = Offset.zero & (initialSize / window.devicePixelRatio);
+    final paintBounds = Offset.zero & Size(640, 360);
     final recorder = PictureRecorder();
     final canvas = Canvas(recorder, paintBounds);
-    canvas.translate(paintBounds.width / 2.0, paintBounds.height / 2.0);
 
     final delta = previous == Duration.zero ? Duration.zero : now - previous;
     previous = now;
-    final t = delta.inMicroseconds / Duration.microsecondsPerSecond;
+    final t = delta.inMicroseconds / 1000000; // Duration.microsecondsPerSecond;
 
     world.update(t);
     world.render(t, canvas);
@@ -70,14 +50,17 @@ main() async {
 
   window.onPointerDataPacket = (packet) {
     final pointer = packet.data.first;
-    world.input(pointer.physicalX, pointer.physicalY);
+    world.input(
+      pointer.physicalX / pixelRatio,
+      pointer.physicalY / pixelRatio,
+    );
   };
 }
 
 class World {
   var _turn = 0.0;
-  double _x = 0;
-  double _y = 0;
+  double _x = 320;
+  double _y = 180;
   static const rotationsPerSecond = 0.25;
 
   // World(this._x, this._y);
@@ -85,8 +68,10 @@ class World {
 
   void input(double x, double y) {
     print('$x, $y');
-    _x = x - 640;
-    _y = y - 360;
+    // _x = x - 640;
+    // _y = y - 360;
+    _x = x;
+    _y = y;
   }
 
   void update(double t) {
@@ -97,7 +82,8 @@ class World {
     // canvas.drawPaint(Paint()..color = Color(0xff880000));
     canvas.save();
     canvas.translate(_x, _y);
-    canvas.rotate(tau * _turn);
+    // canvas.translate(320, 180);
+    // canvas.rotate(tau * _turn);
     var white = Paint()..color = Color(0xffffffff);
     var size = 100.0;
     canvas.drawRect(Rect.fromLTWH(-size / 2, -size / 2, size, size), white);
