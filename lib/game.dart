6f09eeec6b5caab509b0e5ab5312c6204c486282
Daniel Anton Mata <anton.mata@gmail.com>
2019-03-13 05:07:20 +0800

    Adjusted buttons

--- lib/game.dart
+++ lib/game.dart
@@ -27,8 +27,7 @@ class Game {
         stfL = btn(1),
         stfR = btn(3),
         rotL = btn(4),
-        rotR = btn(5),
-        shoot = btn(6);
+        rotR = btn(5);
 
     var move = _moveSpeed * t,
         rot = _rotSpeed * t,
